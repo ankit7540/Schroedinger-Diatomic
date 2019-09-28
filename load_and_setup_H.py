a02f7b427eff88fdf1a2c345ce45a974f4286304
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2019-09-29 00:17:11 +0800

    code  corrected for  unit change

--- load_and_setup_H.py
+++ load_and_setup_H.py
@@ -8,7 +8,66 @@ Created on Sat Sep 28 01:20:57 2019
 # Load necessary modules
 import sys
 import numpy as np
-from scipy import integrate
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+
+#********************************************************************
+# python function to compute the first derivative at the first point
+# and the last point of an array. For this computation, the first 4
+# points are used for the derivative for the first data point. Similarly
+# last 4 (x,y) points are used for the derivative at the last point.
+
+def fd_ends(x, y):
+    ''' Parameter:
+        x       =       xaxis of the array
+        y       =       y data of the array
+                (x and y arrays must have atleast 4 elements)
+        Returns = first derivative at the first point and the last point
+    '''
+    if (len(x) < 4 or len(y) < 4):
+        print("Error : x and y arrays must have 4 elements")
+
+    subx = np.zeros(4)
+    suby = np.zeros(4)
+
+    for i in range(0, 4):
+        subx[i] = x[i]
+        suby[i] = y[i]
+
+    fd1 = ((subx[1]*subx[3]+subx[2]*subx[3]+subx[1]*subx[2]-2*subx[0]*subx[1] \
+          -2*subx[0]*subx[3]-2*subx[0]*subx[2]+3*subx[0]**2)/(-subx[3]+subx[0]) \
+          /(-subx[1]+subx[0])/(-subx[2]+subx[0])*suby[0]-(-subx[2]+subx[0])    \
+          *(-subx[3]+subx[0])/(subx[1]-subx[3])/(-subx[1]+subx[0])/(subx[1]-subx[2])\
+          *suby[1]+(-subx[1]+subx[0])*(-subx[3]+subx[0])/(subx[2]-subx[3])          \
+          /(subx[1]-subx[2])/(-subx[2]+subx[0])*suby[2]-(-subx[1]+subx[0])          \
+          *(-subx[2]+subx[0])/(subx[2]-subx[3])/(subx[1]-subx[3])/(-subx[3]+subx[0])\
+          *suby[3])
+
+    for i in range(0, 4):
+        subx[i] = x[int(i-4)]
+        suby[i] = y[int(i-4)]
+#        print (i, int(i-4))
+
+    fdn = ((subx[1]-subx[3])*(subx[2]-subx[3])/(-subx[3]+subx[0])/(-subx[1] \
+           +subx[0])/(-subx[2]+subx[0])*suby[0]-(-subx[3]+subx[0])*(subx[2]   \
+           -subx[3])/(subx[1]-subx[3])/(-subx[1]+subx[0])/(subx[1]-subx[2])   \
+           *suby[1]+(-subx[3]+subx[0])*(subx[1]-subx[3])/(subx[2]-subx[3])    \
+           /(subx[1]-subx[2])/(-subx[2]+subx[0])*suby[2]-(-2*subx[0]*subx[3]  \
+           -2*subx[1]*subx[3]-2*subx[2]*subx[3]+subx[0]*subx[1]+subx[0]       \
+           *subx[2]+subx[1]*subx[2]+3*subx[3]**2)/(subx[2]-subx[3])/(subx[1]   \
+           -subx[3])/(-subx[3]+subx[0])*suby[3])
+
+    return(fd1, fdn)
+
+#************************************************************************
+# Define the factorial function
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+#************************************************************************
 
 #-------------------------------------------------------------------
 # Define required functions
@@ -30,6 +89,12 @@ correction3 = np.loadtxt("./data/radiative_57.txt")
 distance_c3 = np.loadtxt("./data/r_wave_57_radiative.txt")
 
 #-------------------------------------------------------------------
+# Units:
+unit_potential = "H"
+unit_c1 = "cm-1"
+unit_c2 = "cm-1"
+unit_c3 = "cm-1"
+#-------------------------------------------------------------------
 
 # Parameters :
 
@@ -48,14 +113,60 @@ step  =  0.005
 #  start and end   value of the potential distance curve
 start  = distance[0]
 end = distance[-1]
-
+rwave=np.arange(start,end,step)
 #-------------------------------------------------------------------
 print(start,end)
 
 # Steps
 
 # interpolate  potential and corrections to  the number of  data points
+derv = fd_ends(distance, potential)
+cs = CubicSpline(distance, potential,bc_type=((1,derv[0]),(1,derv[1])))
+potential_interp = cs(rwave)
+
+
+derv = fd_ends(distance_c1,  correction1)
+cs = CubicSpline(distance_c1,  correction1,bc_type=((1,derv[0]),(1,derv[1])))
+adbc1_interp = cs(rwave)
+
+
+derv = fd_ends(distance_c2,  correction2)
+cs = CubicSpline(distance_c2,  correction2,bc_type=((1,derv[0]),(1,derv[1])))
+adbc2_interp = cs(rwave)
+
+
+derv = fd_ends(distance_c3,  correction3)
+cs = CubicSpline(distance_c3,  correction3,bc_type=((1,derv[0]),(1,derv[1])))
+radc_interp = cs(rwave)
+
 
 # Generate  the final potential
 
+#  Generate final potential based on the  unit
+if  (unit_potential ==  "cm-1"):
+    potential_interp = potential_interp / 219474.631370200000000
+
+if  (unit_c1 ==  "cm-1"):
+    adbc1_interp = adbc1_interp / 219474.631370200000000
+
+if  (unit_c2 ==  "cm-1"):
+    adbc2_interp = adbc1_interp / 219474.631370200000000
+
+if  (unit_c3 ==  "cm-1"):
+    radc_interp = radc_interp / 219474.631370200000000
+
+
+
+fp = potential_interp+ adbc1_interp+adbc2_interp+radc_interp
+
 #  generate the Hmatrix
+
+
+
+
+#-------------------------------------------------------------------
+plt.figure(0)
+ax0 = plt.axes()
+plt.title('Potential', fontsize=20)
+plt.plot( rwave, fp ,'r-',  label='unexposed  pixel')
+#plt.plot( index,  unexposed_pixel2, 'g-',  label='unexposed  pixel')
