cd7fff4c61032ced53104cc3c5bc65d79bb126ad
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2019-09-28 18:06:40 +0800

    data files added

--- load_and_setup_H.py
+++ load_and_setup_H.py
@@ -9,25 +9,25 @@ Created on Sat Sep 28 01:20:57 2019
 import sys
 import numpy as np
 from scipy import integrate
-from scipy.optimize import curve_fit
+
 #-------------------------------------------------------------------
-# Define required functions 
+# Define required functions
 
 #-------------------------------------------------------------------
 
 # Loading data and checks:
 
-distance = np.loadtxt("./data/distance.txt")
-potential = np.loadtxt("./data/potential.txt")
+distance = np.loadtxt("./data/r_wave670_H2.txt")
+potential = np.loadtxt("./data/pes670_H2.txt")
 
-correction1 = np.loadtxt("./data/c1.txt")
-distance_c1 = np.loadtxt("./data/dc1.txt")
+correction1 = np.loadtxt("./data/adiabticE1.txt")
+distance_c1 = np.loadtxt("./data/adb_r_distance.txt")
 
-correction2 = np.loadtxt("./data/c2.txt")
-distance_c2 = np.loadtxt("./data/dc2.txt")
+correction2 = np.loadtxt("./data/adiabticE2.txt")
+distance_c2 = np.loadtxt("./data/adb_r_distance.txt")
 
-correction3 = np.loadtxt("./data/c3.txt")
-distance_c3 = np.loadtxt("./data/dc3.txt")
+correction3 = np.loadtxt("./data/radiative_57.txt")
+distance_c3 = np.loadtxt("./data/r_wave_57_radiative.txt")
 
 #-------------------------------------------------------------------
 
@@ -35,13 +35,27 @@ distance_c3 = np.loadtxt("./data/dc3.txt")
 
 # Atomic number
 q1 = 1
-q1 = 1
+q2 = 1
 
 
 # Atomic mass, in atomic units
 m1 =  918
 m2 = 918
 
+#  step  size
+step  =  0.005
+
+#  start and end   value of the potential distance curve
+start  = distance[0]
+end = distance[-1]
 
 #-------------------------------------------------------------------
+print(start,end)
 
+# Steps
+
+# interpolate  potential and corrections to  the number of  data points
+
+# Generate  the final potential
+
+#  generate the Hmatrix
